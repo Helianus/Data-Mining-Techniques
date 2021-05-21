600e5f818259ff01f4ad0f74dec991f70ba6177f
Hu XinYu <xinyuhelianushu@gmail.com>
2021-05-22 00:12:16 +0800

    update assignment 2

--- assignment02/classifier.py
+++ assignment02/classifier.py
@@ -5,7 +5,6 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
 from xgboost import XGBClassifier
 
 k = 15 # a constant number, you can change it
@@ -28,7 +27,7 @@ def gaussian_naive_bayes(Xtrain, Xtest, ytrain, ytest, flag):
     model = gnb.fit(Xtrain, ytrain)
     ypred = model.predict(Xtest)
     
-    accscore = accuracy_score(ytest, ypred)
+    accscore = accuracy_score(ytest, ypred).mean()
     evalmark = cross_val_score(model, Xtrain, ytrain, cv=k, scoring="accuracy").mean()
     if flag == "True":
         print("Gaussian Naive Bayes: cross vali score", evalmark, ", accuracy score", accscore)
@@ -53,7 +52,7 @@ def decision_tree(Xtrain, Xtest, ytrain, ytest, flag):
     model = clf.fit(Xtrain, ytrain)
     ypred = model.predict(Xtest)
     
-    accscore = accuracy_score(ytest, ypred)
+    accscore = accuracy_score(ytest, ypred).mean()
     evalmark = cross_val_score(model, Xtrain, ytrain, cv=k, scoring="accuracy").mean()
     if flag == "True":
         print("Decision Tree: cross vali score", evalmark, ", accuracy score", accscore)
@@ -79,7 +78,7 @@ def logistic_regression(Xtrain, Xtest, ytrain, ytest, flag):
     model = lr.fit(Xtrain, ytrain)
     ypred = model.predict(Xtest)
     
-    accscore = accuracy_score(ytest, ypred)
+    accscore = accuracy_score(ytest, ypred).mean()
     evalmark = cross_val_score(model, Xtrain, ytrain, cv=k, scoring="accuracy").mean()
     if flag == "True":
         print("Logistic Regression: cross vali score", evalmark, ", accuracy score", accscore)
@@ -104,7 +103,7 @@ def k_nearest_neighbours(Xtrain, Xtest, ytrain, ytest, flag):
     model = knn.fit(Xtrain, ytrain)
     ypred = model.predict(Xtest)
     
-    accscore = accuracy_score(ytest, ypred)
+    accscore = accuracy_score(ytest, ypred).mean()
     evalmark = cross_val_score(model, Xtrain, ytrain, cv=k, scoring="accuracy").mean()
     if flag == "True":
         print("K Nearest Neighbours: cross vali score", evalmark, ", accuracy score", accscore)
@@ -130,7 +129,7 @@ def support_vector_machine(Xtrain, Xtest, ytrain, ytest, flag):
     model = svm.fit(Xtrain, ytrain)
     ypred = model.predict(Xtest)
     
-    accscore = accuracy_score(ytest, ypred)
+    accscore = accuracy_score(ytest, ypred).mean()
     evalmark = cross_val_score(model, Xtrain, ytrain, cv=k, scoring="accuracy").mean()
     if flag == "True":
         print("Support Vector Machine: cross vali score", evalmark, ", accuracy score", accscore)
@@ -155,63 +154,13 @@ def stochastic_gradient_descent(Xtrain, Xtest, ytrain, ytest, flag):
     model = sgd.fit(Xtrain, ytrain)
     ypred = model.predict(Xtest)
     
-    accscore = accuracy_score(ytest, ypred)
+    accscore = accuracy_score(ytest, ypred).mean()
     evalmark = cross_val_score(model, Xtrain, ytrain, cv=k, scoring="accuracy").mean()
     if flag == "True":
         print("Stochastic Gradient Descent: cross vali score", evalmark, ", accuracy score", accscore)
     
     return ypred
 
-def random_forest(Xtrain, Xtest, ytrain, ytest, flag):
-    """
-    Random Forest method
-    
-    param：
-        Xtrain: input train data
-        Xtest: input test data
-        ytrain: output train data
-        ytest: output test data
-        flag: True for print accuracy score, False for not
-
-    return:
-        ypred: result of the predict model
-    """
-    rfr = RandomForestRegressor(n_jobs=-1, random_state=0)
-    model = rfr.fit(Xtrain, ytrain)
-    ypred = model.predict(Xtest)
-    
-    accscore = accuracy_score(ytest, ypred)
-    evalmark = cross_val_score(model, Xtrain, ytrain, cv=k, scoring="accuracy").mean()
-    if flag == "True":
-        print("Random Forest: cross vali score", evalmark, ", accuracy score", accscore)
-    
-    return ypred
-
-
-def ada_boost(Xtrain, Xtest, ytrain, ytest, flag):
-    """
-    Ada Boost method
-    
-    param：
-        Xtrain: input train data
-        Xtest: input test data
-        ytrain: output train data
-        ytest: output test data
-        flag: True for print accuracy score, False for not
-
-    return:
-        ypred: result of the predict model
-    """
-    abr = AdaBoostRegressor(learning_rate=0.05, loss="linear", n_estimators=100, random_state=0)
-    model = abr.fit(Xtrain, ytrain)
-    ypred = model.predict(Xtest)
-    
-    accscore = accuracy_score(ytest, ypred)
-    evalmark = cross_val_score(model, Xtrain, ytrain, cv=k, scoring="accuracy").mean()
-    if flag == "True":
-        print("Ada Boost: cross vali score", evalmark, ", accuracy score", accscore)
-    
-    return ypred
 
 def xg_boost(Xtrain, Xtest, ytrain, ytest, flag):
     """
@@ -231,7 +180,7 @@ def xg_boost(Xtrain, Xtest, ytrain, ytest, flag):
     model = xgb.fit(Xtrain, ytrain)
     ypred = model.predict(Xtest)
     
-    accscore = accuracy_score(ytest, ypred)
+    accscore = accuracy_score(ytest, ypred).mean()
     evalmark = cross_val_score(model, Xtrain, ytrain, cv=k, scoring="accuracy").mean()
     if flag == "True":
         print("XGBoost: cross vali score", evalmark, ", accuracy score", accscore)
